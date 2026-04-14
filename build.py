57e764742cac3af453081ca86c9c0290446f4112
Nikita <youremail@example.com>
2026-04-14 10:09:15 +0000

    release 1.0.6

--- build.py
+++ build.py
@@ -1,10 +1,13 @@
 import PyInstaller.__main__
 import os
+from pathlib import Path
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = Path(__file__).resolve().parent
+ASSETS_DIR = BASE_DIR / "assets"
+MAIN_FILE = BASE_DIR / "main.py"
 
 PyInstaller.__main__.run([
-    os.path.join(BASE_DIR, 'main.py'),
+    str(MAIN_FILE),
 
     '--onefile',
     '--windowed',
@@ -13,7 +16,7 @@ PyInstaller.__main__.run([
 
     '--name=Multiaccount',
 
-    '--add-data=assets;assets',
+    f'--add-data={ASSETS_DIR};assets',
 
     '--distpath=dist',
     '--workpath=build',
